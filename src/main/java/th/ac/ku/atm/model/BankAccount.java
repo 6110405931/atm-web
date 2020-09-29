0914a719d4ba269e0f7a9f9f7349e6589f4d1eeb
kt_boom <kritsada.tan@ku.th>
2020-09-29 17:01:16 +0700

    connect api get

--- src/main/java/th/ac/ku/atm/model/BankAccount.java
+++ src/main/java/th/ac/ku/atm/model/BankAccount.java
@@ -1,5 +1,8 @@
 package th.ac.ku.atm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BankAccount {
 
     private int id;
@@ -28,5 +31,31 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", type='" + type + '\'' +
+                ", balance=" + balance +
+                '}';
     }
 }
