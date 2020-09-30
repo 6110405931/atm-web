0fa43bd632533c41d0368ed06136d20623471b31
kt_boom <kritsada.tan@ku.th>
2020-09-30 22:24:07 +0700

    connect api post

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -4,7 +4,10 @@ package th.ac.ku.atm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
 
 @Controller
@@ -16,13 +19,27 @@ public class BankAccountController {
     public BankAccountController(BankAccountService bankAccountService) {
         this.bankAccountService = bankAccountService;
     }
+
     @GetMapping
-    public String getBankAccountPage(Model model){
+    public String getBankAccountPage(Model model) {
+        model.addAttribute("allBankAccounts", bankAccountService.getBankAccounts());
+        return "bankaccount";
+    }
+
+    @PostMapping
+    public String openAccount(@ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.openAccount(bankAccount);
+        model.addAttribute("allBankAccounts",bankAccountService.getBankAccounts());
+        return "redirect:bankaccount";
+    }
+
+//    @GetMapping
+//    public String getBankAccountPage(Model model){
 
 //        model.addAttribute("allBankAccounts",bankAccountService.getBankAccountList());
 
-        return "bankaccount";
-    }
+//        return "bankaccount";
+//    }
 
 //    @PostMapping
 //    public String registerBankAccount(@ModelAttribute BankAccount bankAccount,Model model){
